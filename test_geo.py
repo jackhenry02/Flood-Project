49947a41071617526e4faaece3663b8296c28ac3
Cian Phillips <cianphillips@dhcp-10-249-190-163.eduroam.wireless.private.cam.ac.uk>
2023-02-08 12:40:15 +0000

    Complete Tasks and pytests (check)

--- test_geo.py
+++ test_geo.py
@@ -5,40 +5,53 @@ from floodsystem.station import MonitoringStation
 
 # Creating a test station list
 coord = (-2.0, 4.0)
-s1 = MonitoringStation('s_id1', 'm_id1', 'label1', (-2.0,4.0), 'range1', 'river1', 'town1')
-s2 = MonitoringStation('s_id2', 'm_id2', 'label2', (3.0,2.0), 'range2', 'river2', 'town2')
-s3 = MonitoringStation('s_id3', 'm_id3', 'label3', (1.6,-1.6), 'range3', 'river3', 'town3')
-s4 = MonitoringStation('s_id4', 'm_id4', 'label4', (0.1,0.1), 'range4', 'river3', 'town4')
-test_list = [s1,s2,s3,s4]
+stations = [
+    MonitoringStation('s_id1', 'm_id1', 'label1', (-2.0,4.0), (0.0073, 0.14), 'river1', 'town1'),
+    MonitoringStation('s_id2', 'm_id2', 'label2', (3.0,2.0), (1.12, 1.12), 'river2', 'town2'),
+    MonitoringStation('s_id3', 'm_id3', 'label3', (1.6,-1.6), (24.94, 7.51), 'river2', 'town3'),
+    MonitoringStation('s_id4', 'm_id4', 'label4', (0.1,0.1), None, 'river3', 'town4'),
+    MonitoringStation('s_id5', 'm_id5', 'label5', (0.1,0), (9.04, 11.68), 'river3', 'town5')
+]
+
 
 def test_stations_by_distance():
-    output = stations_by_distance(test_list,(0,0))
+    output = stations_by_distance(stations,(0,0))
     #using haversine
-    d1 = haversine (s1.coord, (0,0))
-    d2 = haversine (s2.coord, (0,0))
-    d3 = haversine (s3.coord, (0,0))
-    d4 = haversine (s4.coord, (0,0))
-    assert output == [(s4,d4),(s3,d3),(s2,d2),(s1,d1)]   #checks value and order
+    output_list = []
+    for i in range(len(stations)):
+        output_list.append((stations[len(stations) - (i + 1)], haversine(stations[len(stations) - (i + 1)].coord, (0,0))))
+    assert output == output_list                        #checks value and order
     assert type(output) == list                          #checks type
-    assert len(output) == len(test_list)                 #checks length
-    # Note for future tests: d1,d2,d3,d4 = 497.19868760742435, 400.8631781687255, 251.58939201339484, 15.725355061691959
+    assert len(output) == len(stations)                 #checks length
+    # Note for future tests: d1,d2,d3,d4,d5 = 497.19868760742435, 400.8631781687255, 251.58939201339484, 15.725355061691959, 11.11950802335329
 
 def test_stations_within_radius():
-    output = stations_within_radius(test_list,(0,0),260)
-    d3 = haversine (s3.coord, (0,0))
-    d4 = haversine (s4.coord, (0,0))
-    assert output == [s4,s3]            #checks correct output
+    output = stations_within_radius(stations,(0,0),260)
+    d3 = haversine (stations[2].coord, (0,0))
+    d4 = haversine (stations[3].coord, (0,0))
+    d5 = haversine (stations[4].coord, (0,0))
+
+    assert output == [stations[4],stations[3],stations[2]]    #checks correct output
     assert type(output) == list         #checks type
-    assert len(output) == 2             #checks length 
+    assert len(output) == 3             #checks length 
 
 def test_rivers_with_station():
-    output = rivers_with_station(test_list)
+    output = rivers_with_station(stations)
     assert output == {'river1','river2','river3'}       #checks correct output
     assert type(output) == set                          #checks type
     assert len(output) == 3                             #checks length (should ommitt duplicate)
 
 def test_stations_by_river():
-    output = stations_by_river(test_list)
-    assert output == {'river1':['label1'],'river2':['label2'],'river3':['label3','label4']}      #checks correct output
+    output = stations_by_river(stations)
+    assert output == {'river1':['label1'],'river2':['label2','label3'],'river3':['label4','label5']}      #checks correct output
     assert type(output) == dict                                                                  #checks type
     assert len(output) == 3                                                                      #checks length
+
+def test_rivers_by_station_number():
+    """Assert that the function works correctly, especially the case when the number of stations on a set of rivers are equal"""
+    
+    rivers = rivers_by_station_number(stations, 1)
+
+    assert rivers == [('river2', 2), ('river3', 2)]     # Expected output
+    assert type(rivers) == list
+    assert len(rivers) == 2
--- test_station.py
+++ test_station.py
@@ -3,8 +3,15 @@
 # SPDX-License-Identifier: MIT
 """Unit test for the station module"""
 
-from floodsystem.station import MonitoringStation
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 
+stations = [
+    MonitoringStation('s_id1', 'm_id1', 'label1', (-2.0,4.0), (0.0073, 0.14), 'river1', 'town1'),
+    MonitoringStation('s_id2', 'm_id2', 'label2', (3.0,2.0), (1.12, 1.12), 'river2', 'town2'),
+    MonitoringStation('s_id3', 'm_id3', 'label3', (1.6,-1.6), (24.94, 7.51), 'river2', 'town3'),
+    MonitoringStation('s_id4', 'm_id4', 'label4', (0.1,0.1), None, 'river3', 'town4'),
+    MonitoringStation('s_id5', 'm_id5', 'label5', (0.1,0), (9.04, 11.68), 'river3', 'town5')
+]
 
 def test_create_monitoring_station():
 
@@ -25,3 +32,12 @@ def test_create_monitoring_station():
     assert s.typical_range == trange
     assert s.river == river
     assert s.town == town
+
+def test_inconsistent_typical_range_stations():
+    """Assert to confirm whether this function is working correctly, taking into consideration the cases when the typical value is none and when its proportions are incorrect."""
+    
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+
+    assert inconsistent_stations == [stations[2], stations[3]]      # Expected output
+    assert type(inconsistent_stations) == list
+    assert len(inconsistent_stations) == 2
