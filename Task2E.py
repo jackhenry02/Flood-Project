14ff2444efb5dedd36ffcfcec9b14bcc49da87f0
Cian Phillips <cianphillips@dhcp-10-249-179-226.eduroam.wireless.private.cam.ac.uk>
2023-02-24 22:01:54 +0000

    Task2E Complete

--- Task2E.py
+++ Task2E.py
@@ -1,19 +1,21 @@
 
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.plot import plot_water_levels
+from floodsystem.flood import stations_highest_rel_level
 import datetime
 
 def run():
     """Requirements for Task 2E"""
     stations=build_station_list()  # Makes list of stations
-    
-    
-    dates, levels = fetch_measure_levels(stations[0].measure_id, datetime.timedelta(days=10))
-    
-    plot_water_levels(stations[0], dates, levels)
-    
+    update_water_levels(stations)
+    number_of_stations = 5
 
+    stations_highest_risk = stations_highest_rel_level(stations, number_of_stations)
+
+    for station in stations_highest_risk:
+        dates, levels = fetch_measure_levels(station[0].measure_id, datetime.timedelta(days=10))
+        plot_water_levels(station[0], dates, levels)
 
 
 
